25b1643e9f5013524e4f90d29780f039b2cf0e4a
katherineleeyq <katherineleeyq@163.com>
2019-03-11 01:23:53 +0800

    add solutions

---
+++ src/leetcode/PendingSolution407.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class PendingSolution407 {
+    public int trapRainWater(int[][] heightMap) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[][] heightMap = {{1,4,1,1,3,2},{3,2,1,3,2,4},{2,3,3,2,3,1}};
+        PendingSolution407 s = new PendingSolution407();
+        s.trapRainWater(heightMap);
+    }
+}
---
+++ src/leetcode/Solution653.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
+public class Solution653 {
+    public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> valueSet = new HashSet<>();
+
+
+
+        return false;
+    }
+}
